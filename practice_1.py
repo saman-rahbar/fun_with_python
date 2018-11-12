ffe9db12ddf38c41df7929820b24cf38f909d809
Sam <srahbar12@hotmail.com>
2018-11-12 08:35:21 -0600

    new example added

--- practice_1.py
+++ practice_1.py
@@ -38,3 +38,29 @@ example_1.twoSum([1, 4, 15, 3], 7)
 
 
  
+#Question_2
+
+"""Finding the longest path in a graph assuming we know the starting point
+
+for this purpose DFS is implemented"""
+
+def DFS(G, v, seen = None, path = None):
+	if seen is None: seen = []
+	if path is None: path = [v]
+	seen.append(v)
+	paths = []
+	for t in G[v]:
+		if t not in seen:
+			t_path = path + [t]
+			paths.append(tuple(t_path))
+			paths.extend(DFS(G,t, seen = [:], t_path))
+	
+	return paths
+
+
+G  = defaultdict(list)
+for (s, t) in edges:
+	G[s].append(t)
+	G[t].append(s)
+
+
